adee92cf606167a9a1c3e5cad086158a6970fc5d
spatialflux <frankservaar@gmail.com>
2025-01-24 16:38:19 +0100

    Added parallel pipline test files

---
+++ tests/test_generate_funscript_3.py
@@ -0,0 +1,11 @@
+import os
+
+from script_generator.scripts.generate_funscript import generate_funscript
+from script_generator.state.app_state import AppState
+
+os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
+
+app_state = AppState(is_cli=True)
+app_state.video_path = "C:/cvr/funscript-generator/test.mp4"
+app_state.frame_start = 60*60*20
+generate_funscript(app_state)
