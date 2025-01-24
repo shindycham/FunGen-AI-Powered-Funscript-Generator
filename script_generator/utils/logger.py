1ff6c04fc7c9583881bbc0d1b5f309b9839e7063
ack00gar <arnaud.chevreux@gmail.com>
2025-01-24 17:27:40 +0100

    added UTF8 for non latin encodings

--- script_generator/utils/logger.py
+++ script_generator/utils/logger.py
@@ -16,7 +16,7 @@ logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s|%(levelname)s|%(name)s|%(message)s",
     handlers=[
-        logging.FileHandler("FSGenerator.log", mode="w"),
+        logging.FileHandler("FSGenerator.log", mode="w", encoding='utf-8'),
         ColorizedStreamHandler(sys.stdout)
     ]
 )
