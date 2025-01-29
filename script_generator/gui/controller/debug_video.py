b7c31c6831d025449011c804fdb11c8caf4e1783
ack00gar <arnaud.chevreux@gmail.com>
2025-01-29 21:47:55 +0100

    temp fix for mac platforms

--- script_generator/gui/controller/debug_video.py
+++ script_generator/gui/controller/debug_video.py
@@ -1,6 +1,7 @@
 import os
 import threading
 from tkinter import messagebox
+import platform
 
 from script_generator.debug.video_player.play import play_debug_video
 from script_generator.object_detection.util.utils import get_metrics_file_info
@@ -9,7 +10,24 @@ from script_generator.utils.file import get_output_file_path
 from script_generator.debug.logger import logger
 
 def debug_video(state: AppState):
-    def run():
+    if platform.system() != "Darwin":
+        def run():
+            if not state.video_path:
+                messagebox.showerror("Error", "Please select a video file.")
+                return
+
+            state.set_video_info()
+
+            exists, path, filename = get_metrics_file_info(state)
+            if exists:
+                play_debug_video(state=state, start_frame=state.frame_start)
+            else:
+                logger.error(f"Debug logs file not found: {path}")
+                messagebox.showinfo("Info", f"Debug logs file not found: {filename}")
+
+        processing_thread = threading.Thread(target=run)
+        processing_thread.start()
+    else:
         if not state.video_path:
             messagebox.showerror("Error", "Please select a video file.")
             return
@@ -22,6 +40,3 @@ def debug_video(state: AppState):
         else:
             logger.error(f"Debug logs file not found: {path}")
             messagebox.showinfo("Info", f"Debug logs file not found: {filename}")
-
-    processing_thread = threading.Thread(target=run)
-    processing_thread.start()
