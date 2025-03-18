8a1c1cd212b984a0384f151b5cf8a926ab8bbd69
k00gar <arnaud.chevreux@gmail.com>
2025-03-18 20:36:08 +0100

    Temp fix for gauge display in live preview

--- script_generator/debug/video_player/overlay_widgets.py
+++ script_generator/debug/video_player/overlay_widgets.py
@@ -14,19 +14,7 @@ class OverlayWidgets:
         return image
 
     @staticmethod
-    def old_draw_gauge(image, distance, x=15, y=20):
-        gauge_width = 12
-        gauge_height = 100
-        gauge_x = image.shape[1] - x
-        gauge_y = image.shape[0] - 100 - y
-        cv2.rectangle(image, (gauge_x, gauge_y), (gauge_x + gauge_width, gauge_y + gauge_height), (0, 0, 0), -1)
-        fill_height = int((distance / 100) * gauge_height)
-        cv2.rectangle(image, (gauge_x, gauge_y + gauge_height - fill_height), (gauge_x + gauge_width, gauge_y + gauge_height), (0, 255, 0), -1)
-        cv2.putText(image, str(int(distance)), (gauge_x, gauge_y + gauge_height - fill_height - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.25, (0, 255, 0), 1)
-        return image
-
-    @staticmethod
-    def draw_gauge(image, distance, x, y):
+    def draw_gauge(image, distance, x=15, y=20):
         distance = max(0, min(100, distance))
         gauge_width = 12
         gauge_height = 100
